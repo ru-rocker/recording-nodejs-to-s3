b7ea2baf3f2b0cf846e4523ef9931667afda8068
ru rocker <ru.rocker81@gmail.com>
2022-07-25 11:11:14 +0700

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ app.get('/', (req, res) => {
 });
 app.use('/record', uploadRouter);
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`App listening at http://localhost:${port}`);
 });
