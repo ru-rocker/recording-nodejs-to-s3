72817c534fb37181cdc161d45d63ba230e34b3f3
ru rocker <ru.rocker81@gmail.com>
2022-07-25 11:12:04 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 const uploadRouter = require('./routes/upload');
 
